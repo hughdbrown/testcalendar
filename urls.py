a52488ec39eb3087d5e172d8854d5655dbbc535b
Hugh Brown <hughdbrown@yahoo.com>
2009-06-17 00:42:54 -0400

    Shorten urlpath

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('',
-	url(regex=r'^gencal/$', view="testcalendar.views.foo", kwargs=None, name="foo"),
-	url(regex=r'^simple-gencal/$', view="testcalendar.views.bar", kwargs=None, name="bar"),
-	url(regex=r'^(?P<year>\d{4})/(?P<month>\d+)/$', view='testcalendar.views.index', kwargs=None, name='gencal'),
+urlpatterns = patterns('testcalendar.views',
+	url(regex=r'^gencal/$', view="foo", kwargs=None, name="foo"),
+	url(regex=r'^simple-gencal/$', view="bar", kwargs=None, name="bar"),
+	url(regex=r'^(?P<year>\d{4})/(?P<month>\d+)/$', view='index', kwargs=None, name='gencal'),
 )
